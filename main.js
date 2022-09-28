2302c3669c2ce6c33e07e6dc13cfbe3aaf01a200
Pi-Mito <107584926+Pi-Mito@users.noreply.github.com>
2022-09-28 17:02:10 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var first_name = "Ranbir";
+var last_name = "Kapoor";
+var nome_total = first_name.concat(last_name)
+console.log(nome_total);
+var a = 10;
+var b = 5;
+var c = a - b;
+var d = a + b;
+var e = a / b;
+console.log(c);
+console.log(d);
+console.log(e);
